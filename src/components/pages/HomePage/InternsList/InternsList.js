d64759ea79d1d518438d338684eb135a949ebc39
Anton Skidan <web.dew555@gmail.com>
2024-11-07 03:56:24 +0200

    Merge InternsList: updating the branch with develop

--- src/components/pages/HomePage/InternsList/InternsList.js
+++ src/components/pages/HomePage/InternsList/InternsList.js
@@ -1,8 +1,9 @@
-import { memo, useState } from 'react';
+import React, { useState, useEffect, memo } from 'react';
 import { useSelector } from 'react-redux';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
-import Modal from '@commons/Modal/Modal';
+import { v4 as uuidv4 } from 'uuid';
+import Modal from '@commons/Modal';
 import InternDetailModal from '@pages/HomePage/InternDetailModal';
 import EditInternModal from '@pages/HomePage/InternModal';
 import s from './InternsList.module.scss';
@@ -15,6 +16,69 @@ const InternsList = ({
 }) => {
   const interns = useSelector(state => state.interns.interns);
   const [selectedIntern, setSelectedIntern] = useState(null);
+  const [filterText, setFilterText] = useState('');
+  const [visibleFields, setVisibleFields] = useState({
+    internshipType: true,
+    internshipStage: true,
+    startDate: true,
+  });
+
+  const handleFilterChange = e => {
+    setFilterText(e.target.value);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+
+      setVisibleFields({
+        internshipType: width > 768,
+        internshipStage: width > 992,
+        startDate: width > 1024,
+      });
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const filteredInterns = interns.filter(intern => {
+    return (
+      intern.fullName.toLowerCase().includes(filterText.toLowerCase()) ||
+      intern.direction.toLowerCase().includes(filterText.toLowerCase()) ||
+      (visibleFields.internshipType &&
+        intern.internshipType
+          .toLowerCase()
+          .includes(filterText.toLowerCase())) ||
+      (visibleFields.internshipStage &&
+        intern.internshipStage
+          .toLowerCase()
+          .includes(filterText.toLowerCase())) ||
+      (visibleFields.startDate &&
+        intern.startDate.toLowerCase().includes(filterText.toLowerCase()))
+    );
+  });
+
+  const highlightMatch = (text, filter) => {
+    if (!filter) return text;
+
+    const regex = new RegExp(`(${filter})`, 'gi');
+    const parts = text.split(regex);
+
+    return parts.map(part =>
+      part.toLowerCase() === filter.toLowerCase() ? (
+        <span key={uuidv4()} className={s.highlight}>
+          {part}
+        </span>
+      ) : (
+        <React.Fragment key={uuidv4()}>{part}</React.Fragment>
+      ),
+    );
+  };
 
   const onDetailClick = intern => {
     setSelectedIntern(intern);
@@ -31,56 +95,78 @@ const InternsList = ({
       {!interns.length ? (
         <p className={s.emptyList}>Список пуст</p>
       ) : (
-        interns.map(intern => (
-          <div key={intern.id} className={s.intern}>
-            <p className={s.internItem}>
-              <strong className={clsx(s.label, s.fullNameLabel)}>ФИО:</strong>
-              <span>{intern.fullName}</span>
-            </p>
-            <p className={s.internItem}>
-              <strong className={clsx(s.label, s.directionLabel)}>
-                Направление:
-              </strong>
-              <span>{intern.direction}</span>
-            </p>
-            <p className={clsx(s.internItem, s.internshipType)}>
-              <strong className={s.label}>Тип стажировки:</strong>
-              <span>{intern.internshipType}</span>
-            </p>
-            <p className={clsx(s.internItem, s.internshipStage)}>
-              <strong className={s.label}>Стадия стажировки:</strong>
-              <span>{intern.internshipStage}</span>
-            </p>
-            <p className={clsx(s.internItem, s.startDate)}>
-              <strong className={s.label}>Дата начала:</strong>
-              <span>{intern.startDate}</span>
-            </p>
-            <div className={s.buttons}>
-              <button
-                className={clsx(s.btn, 'icon-eye')}
-                onClick={() => onDetailClick(intern)}
-              ></button>
-              <button
-                className={clsx(s.btn, 'icon-pencil')}
-                onClick={() => onEditClick(intern)}
-              ></button>
-            </div>
+        <>
+          <input
+            type="text"
+            placeholder="Поиск"
+            value={filterText}
+            onChange={handleFilterChange}
+            className={s.filterInput}
+          />
+          {!filteredInterns.length ? (
+            <p className={s.emptyList}>Результаты поиска отсутствуют</p>
+          ) : (
+            filteredInterns.map(intern => (
+              <div key={intern.id} className={s.intern}>
+                <p className={s.internItem}>
+                  <strong className={clsx(s.label, s.fullNameLabel)}>
+                    ФИО:
+                  </strong>
+                  <span>{highlightMatch(intern.fullName, filterText)}</span>
+                </p>
+                <p className={s.internItem}>
+                  <strong className={clsx(s.label, s.directionLabel)}>
+                    Направление:
+                  </strong>
+                  <span>{highlightMatch(intern.direction, filterText)}</span>
+                </p>
+                <p className={clsx(s.internItem, s.internshipType)}>
+                  <strong className={s.label}>Тип стажировки:</strong>
+                  <span>
+                    {highlightMatch(intern.internshipType, filterText)}
+                  </span>
+                </p>
+                <p className={clsx(s.internItem, s.internshipStage)}>
+                  <strong className={s.label}>Стадия стажировки:</strong>
+                  <span>
+                    {highlightMatch(intern.internshipStage, filterText)}
+                  </span>
+                </p>
+                <p className={clsx(s.internItem, s.startDate)}>
+                  <strong className={s.label}>Дата начала:</strong>
+                  <span>{highlightMatch(intern.startDate, filterText)}</span>
+                </p>
+                <div className={s.buttons}>
+                  <button
+                    className={clsx(s.btn, 'icon-eye')}
+                    onClick={() => onDetailClick(intern)}
+                  ></button>
+                  <button
+                    className={clsx(s.btn, 'icon-pencil')}
+                    onClick={() => onEditClick(intern)}
+                  ></button>
+                </div>
 
-            <Modal
-              isOpen={isDetailModalOpen}
-              setIsModalOpen={setIsDetailModalOpen}
-            >
-              <InternDetailModal selectedIntern={selectedIntern} />
-            </Modal>
-            <Modal isOpen={isEditModalOpen} setIsModalOpen={setIsEditModalOpen}>
-              <EditInternModal
-                isEditMode
-                selectedIntern={selectedIntern}
-                setIsModalOpen={setIsEditModalOpen}
-              />
-            </Modal>
-          </div>
-        ))
+                <Modal
+                  isOpen={isDetailModalOpen}
+                  setIsModalOpen={setIsDetailModalOpen}
+                >
+                  <InternDetailModal selectedIntern={selectedIntern} />
+                </Modal>
+                <Modal
+                  isOpen={isEditModalOpen}
+                  setIsModalOpen={setIsEditModalOpen}
+                >
+                  <EditInternModal
+                    isEditMode
+                    selectedIntern={selectedIntern}
+                    setIsModalOpen={setIsEditModalOpen}
+                  />
+                </Modal>
+              </div>
+            ))
+          )}
+        </>
       )}
     </div>
   );
